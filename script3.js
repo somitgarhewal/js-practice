6ec66c0d37b409bbc6ecf302709fbb3b9ab96d41
somit <somitgarhewal@gmail.com>
2025-09-13 02:17:29 +0530

    using top-bottoms

--- script3.js
+++ script3.js
@@ -1,16 +1,38 @@
 const cards = document.querySelectorAll('.card')
-console.log('card', cards);
 
-const observer = new IntersectionObserver((entries) => {
-    console.log('entries', entries)
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.style.background = 'green';
+// using Intersection observer
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.style.background = 'green';
+//         } else {
+//             entry.target.style.background = 'red';
+//         }
+//     })
+// }, {
+//     threshold: 1
+// })
+// cards.forEach((card) => observer.observe(card))
+
+
+// using top-bottoms
+
+const scrollContainer = document.querySelector('.cardWrapper')
+
+function handleScroll(){
+    const containerTop = scrollContainer.getBoundingClientRect().top;
+    const containerBottom = scrollContainer.getBoundingClientRect().bottom;
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+        const cardBottom = card.getBoundingClientRect().bottom;
+        if (cardTop >= containerTop && cardBottom <= containerBottom) {
+            card.style.background = 'green';
         } else {
-            entry.target.style.background = 'red';
+            card.style.background = 'red';
         }
-    })
-}, {
-    threshold: 1
-})
-cards.forEach((card) => observer.observe(card))
+        console.log('card', {cardTop, cardBottom, containerTop, containerBottom});
+    });
+}
+
+ scrollContainer.addEventListener('scroll', handleScroll)
+ handleScroll()
